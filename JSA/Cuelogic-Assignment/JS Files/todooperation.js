196dd88b05def74a4123b799f96585df5e9b681a
Akshat Zala <30282789+akshatz@users.noreply.github.com>
2019-07-07 18:46:28 +0530

    Update todooperation.js

--- JSA/Cuelogic-Assignment/JS Files/todooperation.js
+++ JSA/Cuelogic-Assignment/JS Files/todooperation.js
@@ -1,6 +1,10 @@
 var user_array = JSON.parse(localStorage.getItem("userDetails"));
 var session = sessionStorage.user;
 var input = document.createElement("input");
+input.setAttribute("type", "checkbox");
+input.setAttribute("class", "selectedcheckbox");
+var todo_array = user_array[userid].ToDO;
+
 var status ="Pending"; 
 for(var index = 0; index < user_array.length; index++)
 {
@@ -12,48 +16,48 @@ for(var index = 0; index < user_array.length; index++)
   }
 }
 
-var todo_array = user_array[userid].ToDO;
-input.setAttribute("type", "checkbox");
-input.setAttribute("class", "selectedcheckbox");
-var check = document.getElementsByClassName("selectedcheckbox");
 
 //Edit ToDo
 function edit(IdofElement){
-// alert(IdofElement);
-  var todoid = 0;
+  alert(IdofElement)
+  var index, todoid;
   for(var index = 0; index < todo_array.length; index++){
-    if(todo_array.TodoId == IdofElement){
+    if(todo_array[index].TodoId == IdofElement){
       var todoid = index;
-      // alert(todoid);
-      }
-      document.getElementById("description").value = todo_array[todoid].Description;
-      document.getElementById("categories").value = todo_array[todoid].Categories;
-      document.getElementById("due_date").value = todo_array[todoid].due_date;
-      document.getElementById("reminder").value = todo_array[todoid].Reminder;
+      alert(todoid);
+      document.getElementById("description").value = todo_array[index].Description;
+      document.getElementById("categories").value = todo_array[index].Categories;
+      document.getElementById("due_date").value = todo_array[index].due_date;
+      document.getElementById("reminder").value = todo_array[index].Reminder;
+      break;
     }
+    }  
 }
+
+// }
+
 //Save ToDO
 function save(IdofElement){
-  for(var index = 0; index < todo_array.length; index++){
-    if(todo_array[index].TodoId === IdofElement){
-         todoid = index;
+  var index;
+    for(index = 0; index < todo_array.length; index++){
+      if(todo_array[index].TodoId == IdofElement){
+      todoid =index;
+      console.log(todoid);
+      console.log(todo_array[todoid]);  
+      break;
       }
-    var Description2= document.getElementById("description").value;
-    var Categories = document.getElementById("categories").value;
-    var due_date2 = document.getElementById("due_date").value;
-    var Reminder2 = document.getElementById("reminder").value;
+    }
 
-    user_array[userid].ToDO[todoid].Description = Description2;
-    user_array[userid].ToDO[todoid].Categories = Categories;
-    user_array[userid].ToDO[todoid].due_date = due_date2;
-    user_array[userid].ToDO[todoid].Reminder = Reminder2;
-    user_array = JSON.stringify(user_array);
-    localStorage.setItem('userDetails',user_array);
-    
-
-}window.location.reload();
-}
-
+    todo_array[todoid].Description =document.getElementById("description").value;
+      todo_array[todoid].Categories =document.getElementById("categories").value;
+      todo_array[todoid].Due_Date = document.getElementById("due_date").value;
+      todo_array[todoid].Reminder = document.getElementById("reminder").value;
+      console.log(todo_array[todoid].TodoId);
+      user_array = JSON.stringify(user_array);
+      localStorage.setItem('userDetails',user_array);
+      // window.location.reload();
+      window.location ='todo.html';
+   }    
 
 //Filter By Category
 function filter(){
@@ -61,17 +65,38 @@ function filter(){
     display_element(todo_array);
   }
   else if(document.getElementById("filterbycategories").value === "Office"){  
-        var filteredarraybycategory=todo_array.filter(function(searchoffice){
+        var filteredarraybycategory = todo_array.filter(function(searchoffice){
           return (searchoffice.Categories==="Office");
     })
     display_element(filteredarraybycategory);
   }
   else if(document.getElementById("filterbycategories").value === "Personal")
   {    
-    var filteredarraybycategory=todo_array.filter(function(searchoffice){
+    var filteredarraybycategory = todo_array.filter(function(searchoffice){
       return (searchoffice.Categories==="Personal");
-})
-display_element(filteredarraybycategory);
+    })
+  display_element(filteredarraybycategory);
+  }
+}
+
+function filterByStatus(){
+// alert("IN filter by status")
+  if(document.getElementById("filterbystatus").value === "Status"){
+    display_element(todo_array);
+  }
+ else if(document.getElementById("filterbystatus").value === "Pending")
+  {  
+    var filteredArrayByStatus= todo_array.filter(function(searchstatus){
+      return (searchstatus.isDone === "Pending");
+    })
+    display_element(filteredArrayByStatus);
+  }
+else if(document.getElementById("filterbystatus").value === "Done")
+{    
+  var filteredArrayByStatus= todo_array.filter(function(searchstatus){
+    return (searchstatus.isDone === "Done");
+  })
+  display_element(filteredArrayByStatus);
 }
 }
 //Filter by Date
@@ -81,7 +106,7 @@ function filterbyDate(){
   var newsDate=new Date(startDate);
   var dDate =new Date(endDate);
     var filterByDate = todo_array.filter(function(searchtime){
-      return ((new Date(searchtime.Due_Date).getTime() >= newsDate.getTime()) && (new Date(searchtime.Due_Date).getTime() <= dDate.getTime()));
+      return ((new Date(searchtime.Due_Date) >= newsDate) && (new Date(searchtime.Due_Date) <= dDate));
     })
     var a = document.getElementById("table_body");
     var del = a.lastElementChild;
@@ -89,27 +114,30 @@ function filterbyDate(){
       a.removeChild(del);
       del = a.lastElementChild;
     }
-    display_element(filterbyDate);
+    display_element(filterByDate);
   }
 
 
 
-function replaceIsDone(){
-  var input = document.createElement("input")
-  input.setAttribute("type", "checkbox");
-  input.setAttribute("class", "checkbox");
-  var filterbyStatus=todo_array.filter(function(searchBystatus){
-    if(checkbox.checked === true ){
-      return (searchBystatus.status.replace("Pending", "done"));
+function done(IdofElement){
+  //alert(IdofElement);
+  for(var index = 0; index < todo_array.length; index++)
+  {
+    if(todo_array[index].TodoId == IdofElement){
+      todoid = index;
+      console.log(index, todoid);
+      alert(todoid);
+    break;  
     }
-    
-  })
- display_element(filterbyStatus);
- window.location.reload();
-}
-
-// function isPublic(){
-//   var filteredbypublic = todo_array.filter(function(searchByPublic){
-//     return (searchByPublic.isPublic);)
-//   }  
-// }
+  }  
+  todo_array[index].isDone ="Done"
+  user_array = JSON.stringify(user_array);
+  localStorage.setItem('userDetails',user_array);
+  window.location.reload();
+  }
+  // for (var i = 0, length = radios.length; i < length; i++){
+  //  if (radios[i].checked){
+  //   alert(radios[i].value);
+  //   break;
+  //  }
+  // }
